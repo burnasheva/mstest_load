ae76629b3d37ecb2d2fbe7abbf90a4717d7117d8
Nadia Burnasheva@ <nadezhda.burnasheva@gmail.com>
2017-06-29 17:15:33 +0200

    add second test

---
+++ MSTestLoad/SomeTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestLoad
+{
+    [TestClass]
+    public class SomeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
